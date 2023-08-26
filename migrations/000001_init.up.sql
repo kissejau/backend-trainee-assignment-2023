e233808f042121bba4cc3ec9016f7ac2519b36d0
kissejau <kissejau@yandex.ru>
2023-08-26 04:33:26 +0000

    migrations

---
+++ migrations/000001_init.up.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS segments (
+    id SERIAL PRIMARY KEY,
+    slug VARCHAR
+);
+
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR
+);
+
+-- many-to-many
+CREATE TABLE IF NOT EXISTS users_segments (
+    id SERIAL PRIMARY KEY,
+    -- ttl_flag BOOLEAN,
+    -- ttl_deadline TIMESTAMP
+    user_id INTEGER REFERENCES users(id),
+    segment_id INTEGER REFERENCES segments(id)
+);
