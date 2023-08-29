5645484a317f5a448f6e08fd9b970016e7fe7590
kissejau <kissejau@yandex.ru>
2023-08-29 13:31:56 +0000

    logs table, triggers

--- migrations/000001_init.up.sql
+++ migrations/000001_init.up.sql
@@ -8,6 +8,35 @@ CREATE TABLE IF NOT EXISTS users (
     name VARCHAR
 );
 
+CREATE TABLE IF NOT EXISTS logs (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER,
+    slug VARCHAR,
+    type VARCHAR,
+    created_at TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+-- insert data to logs table on new record in users_segments
+CREATE OR REPLACE FUNCTION users_segments_insert_trigger()
+RETURNS TRIGGER AS $$
+BEGIN
+    INSERT INTO logs (user_id, slug, type, created_at)
+    VALUES (NEW.user_id, (SELECT slug FROM segments WHERE id = NEW.segment_id), 'Add', NOW());
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- insert data to logs table on deleting record from users_segments 
+CREATE OR REPLACE FUNCTION users_segments_delete_trigger()
+RETURNS TRIGGER AS $$
+BEGIN
+    INSERT INTO logs (user_id, slug, type, created_at)
+    VALUES (OLD.user_id, (SELECT slug FROM segments WHERE id = OLD.segment_id), 'Delete', NOW());
+    RETURN OLD;
+END;
+$$ LANGUAGE plpgsql;
+
 -- many-to-many
 CREATE TABLE IF NOT EXISTS users_segments (
     id SERIAL PRIMARY KEY,
@@ -16,3 +45,13 @@ CREATE TABLE IF NOT EXISTS users_segments (
     segment_id INTEGER REFERENCES segments(id),
     UNIQUE(user_id, segment_id)
 );
+
+CREATE TRIGGER users_segments_after_insert
+AFTER INSERT ON users_segments
+FOR EACH ROW
+EXECUTE FUNCTION users_segments_insert_trigger();
+
+CREATE TRIGGER users_segments_after_delete
+AFTER DELETE ON users_segments
+FOR EACH ROW
+EXECUTE FUNCTION users_segments_delete_trigger();
